230460a40891ddfe57a6518ea7b29e4997d14d77
gyeri <jeriobcena@gmail.com>
2026-06-06 14:19:44 +0800

    update

---
+++ compress.js
@@ -0,0 +1,21 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const inputDir = './covers';
+const outputDir = './covers_compressed';
+
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
+const files = fs.readdirSync(inputDir).filter(f => f.endsWith('.png'));
+console.log(`Compressing ${files.length} images...`);
+
+Promise.all(
+  files.map((file, i) =>
+    sharp(path.join(inputDir, file))
+      .resize(500, 600, { fit: 'cover' })
+      .jpeg({ quality: 82 })
+      .toFile(path.join(outputDir, file.replace('.png', '.jpg')))
+      .then(() => console.log(`[${i+1}/${files.length}] Done: ${file}`))
+  )
+).then(() => console.log('TAPOS NA!'));
